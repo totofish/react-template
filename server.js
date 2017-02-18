7caf63578e2bc8d21a4d5a9a1016592dcc60ec6d
totofish <eanean120@gmail.com>
2017-02-19 00:45:50 +0800

    node server設定調整

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-const path       = require('path')
-const express    = require('express')
-const app        = express()
-const opener     = require("opener")
-const portfinder = require('portfinder')
-const fs         = require('fs')
-let argv         = require('minimist')(process.argv.slice(2), { boolean:['release'] })
+const path        = require('path')
+const express     = require('express')
+const compression = require('compression')
+const app         = express()
+const opener      = require("opener")
+const portfinder  = require('portfinder')
+const fs          = require('fs')
+let argv          = require('minimist')(process.argv.slice(2), { boolean:['release'] })
+const cache       = {maxAge:'7d'}
 
 let router = express.Router()
 let staticFolder, rootFolder
+router.use(compression())
 if(argv.release) {
   // Production Release
   staticFolder = '/dist'
@@ -19,14 +22,14 @@ if(argv.release) {
 }
 
 // 靜態檔案root目錄
-router.use(express.static(__dirname + staticFolder))
+router.use(express.static(__dirname + staticFolder, cache))
 // 依照html檔名定義router
 fs.readdirSync(__dirname + rootFolder).forEach(function (file) {
   if (fs.statSync(path.join(__dirname + rootFolder, file)).isFile()) {
     if(/.html$/.test(file)) {
       let fileName = file.replace(/.html$/, '')
       router.get(`/${fileName}*`, (req, res, next) => {
-        res.sendFile(path.join(__dirname + rootFolder + file))
+        res.sendFile(path.join(__dirname + rootFolder + file), cache)
       })
     }
   }

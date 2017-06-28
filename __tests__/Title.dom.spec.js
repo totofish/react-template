c6b33c5d947c445738281241a56b522f85f723c6
Totofish <eanean120@gmail.com>
2017-06-28 10:20:14 +0800

    不排除__tests__

---
+++ __tests__/Title.dom.spec.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import {shallow, mount} from 'enzyme';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import Title from '@/components/Title.js'
+
+const routes =[
+  {
+    path     : '',
+    component: '',
+    routes   : [
+      {
+        tagName  : 'Home : Page',
+        path     : `/home/page`,
+        component: '',
+        routes   : [
+          {
+            tagName  : 'Info',
+            path     : `/home/page/info`,
+            component: ''
+          }, {
+            path     : `/home/page/`,
+            component: ''
+          }
+        ]
+      }
+    ]
+  }
+]
+const store = createStore(
+  () => ({
+    sys:{
+      info: {message: 'Info Message'}
+    },
+    routeData: routes
+  })
+)
+
+describe('Title.js', () => {
+  it('<Title /> Info Message是否正確', () => {
+    const titleDom = mount(
+      <Provider store={store}>
+        <Title />
+      </Provider>
+    );
+    expect(titleDom.find('.scene__msg').text()).toEqual('Info Message');
+  });
+
+  it('測試麵包屑第2節點是否正確', () => {
+    const titleDom = mount(
+      <Provider store={store}>
+        <Title jump="/home/page" history={[]} pathname="/home/page/info" />
+      </Provider>
+    );
+    expect(titleDom.find('li').at(1).text()).toEqual('Info');
+  })
+});
